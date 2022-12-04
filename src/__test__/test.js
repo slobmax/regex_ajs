6adb767400a843b32c154bdf4ba25d7a34013561
Maxim Slobozhanin <slob.max@gmail.com>
2022-12-05 03:29:22 +0400

    task completed

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,13 +1,13 @@
-import { userName1, userName2 } from '../app';
+import { user1, user2 } from '../app';
 
-test('Метод validateUsername экземпляра userName1', () => {
-  const received = userName1.validateUsername();
-  const expected = true;
+test('Метод validateUsername экземпляра user1', () => {
+  const received = user1.validateUsername();
+  const expected = 'Никнейм соответствует требованиям';
   expect(received).toBe(expected);
 });
 
-test('Метод validateUsername экземпляра userName2', () => {
-  const received = userName2.validateUsername();
-  const expected = false;
+test('Метод validateUsername экземпляра user2', () => {
+  const received = user2.validateUsername();
+  const expected = 'Некорректный никнейм';
   expect(received).toBe(expected);
 });
--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ class Validator {
   }
 
   validateUsername() {
-    return /^[a-z]([a-z-_]+\d{0,3})*[a-z]+$/.test(this.nikname);
+    let result;
+    if (/^[a-z]([a-z-_]+\d{0,3})*[a-z]+$/.test(this.nikname)) {
+      result = 'Никнейм соответствует требованиям';
+    } else {
+      result = 'Некорректный никнейм';
+    }
+    return result;
   }
 }
 
-export const userName1 = new Validator('user_336-x_545explorer');
-export const userName2 = new Validator('-55user_336-x_545explorer12');
+export const user1 = new Validator('user_336-x_545explorer');
+export const user2 = new Validator('-55user_336-x_545explorer12');
